d699504692668242a4cf83247ddba53e1a27282c
Mohamed Hesham <125760308+Eitsh@users.noreply.github.com>
2024-09-29 16:52:10 +0300

    Add files via upload

---
+++ Task1.dart
@@ -0,0 +1,63 @@
+void main() {
+// int number = 30;
+
+  // if (number % 2 == 0) {
+  //   print('Even');
+  // } else {
+  //   print('Odd');
+  // }
+
+  // List<num> powers = [25, 30, 22, 120, 300, 50];
+  // num max = powers[0];
+
+  // for (num x in powers) {
+  //   if (x > max) {
+  //     max = x;
+  //   }
+  // }
+  // print('$max');
+
+  // List<num> looper = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // num z = looper[0];
+  // for (num z in looper) {
+  //   if (z % 2 != 0) {
+  //     print('$z');
+  //   }
+
+  //   print('$z');
+  // }
+
+  // List<num> sodo = [1, 3, 4, 5, 2, 0, 26];
+  // num v = sodo[0];
+  // for (v in sodo) {
+  //   if (v < 5) {
+  //     print('$v');
+  //   }
+  // }
+
+  // List<num> one = [1, 2, 3, 4, 5, 6, 7, 8];
+  // List<num> second = [1, 2, 3, 8, 22, 24, 25, 55, 70];
+
+  // Set<num> trio = {};
+  // trio.addAll(one);
+  // trio.addAll(second);
+  // print("$trio");
+
+// List <num> v= [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+// List <num>x=[];
+
+// num z= v[0];
+
+//   for(num z in v){
+//     if (z % 2 == 0) {
+// x.add(z);
+// }
+
+//   }          print("$x");
+
+  List<num> heros = [1, 2, 3, 4, 5, 6];
+  num i = heros.length;
+  for (int i = heros.length; i > 0; i--) {
+    print('$i');
+  }
+}
